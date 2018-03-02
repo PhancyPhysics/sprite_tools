56f89dc755f39e6976c45b4298f4da4fef3a8519
PhancyPhysics <kennethianmcintyre@gmail.com>
2018-03-02 15:24:14 -0500

    Added git tag and added download url to setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,22 @@ setup(name='sprite_tools',
     description = 'Used to extract, create, and animate sprites',
     long_description = longDesc,
     url = 'https://github.com/PhancyPhysics/sprite_tools',
+    download_url = 'https://github.com/PhancyPhysics/sprite_tools/archive/0.1.0.tar.gz',
     author = 'Kenneth McIntyre',
     author_email = 'kennethianmcintyre@gmail.com',
     license = 'MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Graphics :: Editors :: Raster-Based',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Natural Language :: English',
+    ],
+    keywords = 'sprite frame animator animation extractor graphics transform',
     install_requires = ['opencv-python','numpy','scipy'],
     packages = ['sprite_tools'],
     include_package_data=True,
     zip_safe = False)
+
+
